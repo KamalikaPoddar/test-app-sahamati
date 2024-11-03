a7ba4b082d7fe94272c94a369628121683faece5
KamalikaPoddar <154564342+KamalikaPoddar@users.noreply.github.com>
2024-11-03 10:22:07 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+const consentController = require('../controllers/consentController');
 
 router.post('/users', userController.createUser);
 router.post('/users/:userId/family-members', userController.addFamilyMember);
 router.post('/users/:userId/financial-accounts', userController.addFinancialAccount);
 router.post('/financial-accounts/:accountId/nominees', userController.addNominee);
 router.post('/users/:userId/api-responses', userController.storeApiResponse);
+
+
+
+router.post('/users/:userId/consent-request', consentController.requestConsent);
+router.get('/consent/:handle/status', consentController.fetchConsentStatus);
+router.post('/consent/:consentHandle/data-request', consentController.triggerDataRequest);
 
 module.exports = router;
