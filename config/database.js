68209911adc4555a6a9c1b9f1fcb7d6e8bf8c6be
KamalikaPoddar <154564342+KamalikaPoddar@users.noreply.github.com>
2024-11-03 10:03:23 +0530

    sahamati access tokens added

--- config/database.js
+++ config/database.js
@@ -14,3 +14,11 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+module.exports = {
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+};
