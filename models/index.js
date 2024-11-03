3b3e63aff3755ea2905ffab382e0c80473fe0133
KamalikaPoddar <154564342+KamalikaPoddar@users.noreply.github.com>
2024-11-03 10:02:46 +0530

    callign the sahamati refresh token every 24 hours

--- models/index.js
+++ models/index.js
@@ -97,3 +97,33 @@ module.exports = {
   Nominee,
   ApiResponse,
 };
+
+
+//COde to refresh the sahamati access token 
+const { Sequelize, DataTypes } = require('sequelize');
+const config = require('../config/database');
+
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: config.dialect,
+});
+
+const ApiResponse = sequelize.define('ApiResponse', {
+  endpoint: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  request_data: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  response_data: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+module.exports = {
+  sequelize,
+  ApiResponse,
+};
